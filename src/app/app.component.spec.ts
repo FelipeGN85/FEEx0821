4ceccc3aa436b46cb50465c0fbb89733a002630c
FelipeBC20 <felipe.bluecerts@gmail.com>
2022-02-10 20:08:43 +0000

    Removed angular deafult page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('feex0821');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('feex0821 app is running!');
-  });
 });
