b875056c1be5793494d7d05c50cc218e3cdda534
FelipeBC20 <felipe.bluecerts@gmail.com>
2022-02-11 09:39:49 +0000

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'generator', pathMatch: 'full' },
+  {path: 'generator'},
+  {path: 'payments'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
