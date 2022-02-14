0730b83147b91fd7cac1785304f59dca5a13e529
FelipeGN85 <felipenascimento1985@gmail.com>
2022-02-14 14:41:46 +0000

    Added live code generator display

--- src/app/payments/payments.component.ts
+++ src/app/payments/payments.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payments.component.css']
 })
 export class PaymentsComponent implements OnInit {
-
+code: string = '';
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => { this.getCode() }, 1 * 1000);
+  }
+  getCode() {
+    this.code = localStorage.getItem('code')?.toString() || '';
   }
 
 }
