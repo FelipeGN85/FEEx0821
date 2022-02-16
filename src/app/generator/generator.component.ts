1b9f003aeb516b03cb7a08139b9250313148ec90
FelipeGN85 <felipenascimento1985@gmail.com>
2022-02-16 17:16:24 +0000

    Test CI

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -21,7 +21,7 @@ export class GeneratorComponent implements OnInit {
 
   ngOnInit(): void {
     setInterval(() => { this.buildGrid() }, 2 * 1000);
-    // this.buildGrid();
+    // this.buildGrid();test ci
   }
 
   buildGrid() {
