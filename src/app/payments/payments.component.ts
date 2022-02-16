e34da77e49e7cb8c56b95d640a887744f3b72e3b
FelipeGN85 <felipenascimento1985@gmail.com>
2022-02-16 10:06:27 +0000

    Change component selector name

--- src/app/payments/payments.component.ts
+++ src/app/payments/payments.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-payments',
+  selector: 'payments',
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css']
 })
